e84ca76a855fcbc7d4712b9a1ff99096cf43803d
chanwhab <chanwhab@gmail.com>
2020-07-07 13:52:00 +0800

    newplugin

--- config/general.php
+++ config/general.php
@@ -10,7 +10,7 @@
 
 return [
     '*' => [
-        'useProjectConfigFile' => true,
+        'useProjectConfigFile' => false,
         'defaultWeekStartDay' => 0,
         'enableCsrfProtection' => true,
         'omitScriptNameInUrls' => true,
