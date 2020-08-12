a36c9e557c2bfed33371619817c6d832ba364c12
chanwhab <chanwhab@gmail.com>
2020-08-12 08:33:15 +0800

    storyupdate

--- src/js/app.js
+++ src/js/app.js
@@ -66,6 +66,35 @@ const swiperSlider = new Swiper('.js-swiper-slider', {
   }
 })
 
+// const swiperSlider = new Swiper('.js-swiper-slider', {
+//   // Optional parameters
+//   // loop: true,
+//   // autoHeight: true,
+//   speed: 300,
+//   watchOverflow: true,
+//   spaceBetween: 48,
+//   slidesPerView: 'auto',
+//   // slidesPerGroup: 3,
+//   grabCursor: true,
+//   freeMode: true,
+//   freeModeSticky: true,
+//   observer: true,
+//   observeParents: true,
+//   mousewheel: {
+//     releaseOnEdges: true,
+//     sensitivity: 1.5
+//   },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     draggable: true
+//   }
+// })
+
 window.setTrueVw = function () {
   let vw = document.documentElement.clientWidth / 100;
   document.documentElement.style.setProperty('--true-vw', `${vw}px`);
