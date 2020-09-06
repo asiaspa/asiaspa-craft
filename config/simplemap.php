f37454bce5bc89a03a047e69edaa57b641d6e0cb
Hong Chan <chanwhab@gmail.com>
2020-09-07 02:50:33 +0800

    spalocatorpush

--- config/simplemap.php
+++ config/simplemap.php
@@ -4,13 +4,13 @@
 use craft\helpers\App;
 use ether\simplemap\enums\MapTiles;
 use ether\simplemap\enums\GeoService;
-
+use ether\simplemap\services\GeoLocationService;
 
 return [
     'mapTiles' => MapTiles::GoogleRoadmap,
     'mapToken' => App::env('GOOGLE_MAPS_API_KEY'),
     'geoService' => GeoService::GoogleMaps,
-    'geoToken' => App::env('GOOGLE_MAPS_GEOSERVICE_KEY')
-    // 'geoLocationService' => GeoLocationService::MaxMindLite,
-    // 'geoLocationToken' => '',
+    'geoToken' => App::env('GOOGLE_MAPS_GEOSERVICE_KEY'),
+    'geoLocationService' => GeoLocationService::IpStack,
+    'geoLocationToken' => App::env('IPSTACK_API_KEY')
 ];
