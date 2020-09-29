99ed747baf20228f51c4967b25aaffb22967b96b
Hong Chan <chanwhab@gmail.com>
2020-09-29 23:20:25 +0800

    adjustimgix

---
+++ config/imager-x-transforms.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'richText' => [
+        'transforms' => [
+            ['width' => 160],
+            ['width' => 320],
+            ['width' => 480],
+            ['width' => 640],
+            ['width' => 800],
+            ['width' => 960],
+            ['width' => 1120],
+            ['width' => 1280],
+            ['width' => 1440],
+            ['width' => 1600],
+            ['width' => 1760],
+            ['width' => 1920]
+        ]
+    ]
+];
